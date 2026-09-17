3972b7f64707093ebc675393a5d57a865b3187a0
Yash Ambarraj Bagal <yashbagal623@gmail.com>
2026-09-18 02:44:09 +0530

    fix: permit public /v3/api-docs path (Swagger + health check)

--- certchain/src/main/java/com/certchain/certchain/security/SecurityConfig.java
+++ certchain/src/main/java/com/certchain/certchain/security/SecurityConfig.java
@@ -65,6 +65,7 @@ public class SecurityConfig {
                     if (swaggerEnabled) {
 
                         auth.requestMatchers(
+                                "/v3/api-docs",
                                 "/v3/api-docs/**",
                                 "/v3/api-docs.yaml",
                                 "/swagger-ui/**",
